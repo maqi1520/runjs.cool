e6e9425fd6f42bd842ecfb3c8bf87cc09673bae5
马琪斌 <maqi1520@163.com>
2022-07-25 00:02:14 +0800

    fix: 🐛 remove preact

--- next.config.js
+++ next.config.js
@@ -20,13 +20,13 @@ const nextConfig = {
   },
   webpack(config, { dev, isServer }) {
     // Replace React with Preact in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     react: "preact/compat",
+    //     "react-dom/test-utils": "preact/test-utils",
+    //     "react-dom": "preact/compat",
+    //   });
+    // }
 
     config.experiments = config.experiments || {};
     Object.assign(config.experiments, {
